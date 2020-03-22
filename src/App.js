fa9733dc336f5e14c773645ad4a269cf75d0dab0
Akram-Mansour <62252350+Akram-Mansour@users.noreply.github.com>
2020-03-22 12:53:10 +0100

    Revert "Functional calendar with css"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from "react";
-import Calendar from "./components/Calendar";
-import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <div id="logo">
-            <span className="icon">date_range</span>
-          </div>
-        </header>
-        <main>
-          <Calendar />
-        </main>
-      </div>
-    );
-  }
+function App() {
+  return <></>;
 }
 
 export default App;
