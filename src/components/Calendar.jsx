cf7bf04ff44db6205644f2637d52d417664d321c
Amin Saad <amin.saad9810@gmail.com>
2020-03-25 16:08:13 +0100

    Made month selector user friendly

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -23,10 +23,8 @@ class Calendar extends Component {
 
     return (
       <div className="header row flex-middle">
-        <div className="col col-start">
-          <div className="icon" onClick={this.prevMonth}>
-            chevron_left
-          </div>
+        <div className="col col-start" onClick={this.prevMonth}>
+          <div className="icon">chevron_left</div>
         </div>
         <div className="col col-center">
           <span>
@@ -43,7 +41,7 @@ class Calendar extends Component {
   renderDays() {
     const dateFormat = "EEEE";
     const days = [];
-    let startDate = startOfWeek(this.state.currentMonth);
+    let startDate = startOfWeek(this.state.currentMonth, { weekStartsOn: 1 });
     for (let i = 0; i < 7; i++) {
       days.push(
         <div className="col col-center" key={i}>
@@ -58,7 +56,7 @@ class Calendar extends Component {
     const { currentMonth, selectedDate } = this.state;
     const monthStart = startOfMonth(currentMonth);
     const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
+    const startDate = startOfWeek(monthStart, { weekStartsOn: 1 });
     const endDate = endOfWeek(monthEnd);
     const dateFormat = "d";
     const rows = [];
@@ -97,6 +95,7 @@ class Calendar extends Component {
     return <div className="body">{rows}</div>;
   }
   onDateClick = day => {
+    console.log(day.getDate());
     this.setState({
       selectedDate: day
     });
